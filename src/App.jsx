94c1a57090296c0dacd33587a3313dedc5b95421
Siddharth <siddharth1360.be21@chitkara.edu.in>
2026-02-14 20:05:56 +0530

    Added Routes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Login from "./pages/Login"
+import OAuthRedirect from "./pages/OAuthRedirect"
+import Dashboard from "./pages/Dashboard"
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login/>} />
+      <Route path="/OAuth" element={<OAuthRedirect/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
+    </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
